3cb1609f9d0741efd1cb8928235fefa870df1d25
Aron Gahagan <gahagan@gmail.com>
2026-05-19 09:43:56 -0400

    fix typo in version-checker.sh

--- version-checker.sh
+++ version-checker.sh
@@ -106,7 +106,7 @@ done
 echo -e "\r\033[K"
 
 {
-  echo "changed module,master,working_scr,working_xml"
+  echo "changed module,master,working_src,working_xml"
   echo -e "$result" | sort
 } | column -t -s ','
 echo
